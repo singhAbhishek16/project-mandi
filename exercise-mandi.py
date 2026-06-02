721e59ed3e60826d4218ccb696571ad1ee6f81b6
abhishek singh <abhishek@abhisheks-MacBook-Air-3.local>
2026-06-02 11:32:01 +0530

    come back here: added vvi todo

--- exercise-mandi.py
+++ exercise-mandi.py
@@ -68,26 +68,28 @@ ps_df["Price Date"] = ps.to_datetime(ps_df["Price Date"])
 # print(len(unique_grades))
 
 
-min_0 = ps_df[ps_df['Min_Price']==0.0]
-max_0 = ps_df[ps_df['Max_Price']==0.0]
-modal_0 = ps_df[ps_df['Modal_Price']==0.0]
-min_max_0 = ps_df[(ps_df['Min_Price']==0.0) & (ps_df['Max_Price']==0.0)]
-min_modal_0 = ps_df[(ps_df['Min_Price']==0.0) & (ps_df['Modal_Price']==0.0)]
-max_modal_0 = ps_df[(ps_df['Max_Price']==0.0) & (ps_df['Modal_Price']==0.0)]
+min_0 = ps_df[(ps_df['Min_Price']==0.0) & (ps_df['Max_Price']!=0.0) & (ps_df['Modal_Price']!=0.0)]
+max_0 = ps_df[(ps_df['Max_Price']==0.0) & (ps_df['Min_Price']!=0.0) & (ps_df['Modal_Price']!=0.0)]
+modal_0 = ps_df[(ps_df['Modal_Price']==0.0) & (ps_df['Max_Price']!=0.0) & (ps_df['Min_Price']!=0.0)]
+min_max_0 = ps_df[(ps_df['Min_Price']==0.0) & (ps_df['Max_Price']==0.0) & (ps_df['Modal_Price']!=0.0)]
+min_modal_0 = ps_df[(ps_df['Min_Price']==0.0) & (ps_df['Modal_Price']==0.0) & (ps_df['Max_Price']!=0.0)]
+max_modal_0 = ps_df[(ps_df['Max_Price']==0.0) & (ps_df['Modal_Price']==0.0) & (ps_df['Min_Price']!=0.0)]
 min_max_modal_0 = ps_df[(ps_df['Max_Price']==0.0) & (ps_df['Modal_Price']==0.0) & (ps_df['Min_Price']==0.0)]
 
-print(f'rows with min price as 0:\n{min_0}')
-print(f'rows with max price as 0:\n{max_0}')
-print(f'rows with modal price as 0:\n{modal_0}')
+print(f'rows with only min price as 0:\n{min_0}')
+print(f'rows with only max price as 0:\n{max_0}')
+print(f'rows with only modal price as 0:\n{modal_0}')
 
-print(f'\nwhole dataset has {ps_df.shape} rows;\n{min_0.shape} are with 0 min;\n{max_0.shape} are with 0 max;\n{modal_0.shape} are with 0 modal;\n{min_max_0.shape} are with min and max 0;\n{min_modal_0.shape} are with min and modal 0;\n{max_modal_0.shape} are with max and modal 0;')
+print(f'\nwhole dataset has {ps_df.shape} rows;\n{min_0.shape} are with 0 min;\n{max_0.shape} are with 0 max;\n{modal_0.shape} are with 0 modal;\n{min_max_0.shape} are with min and max 0;\n{min_modal_0.shape} are with min and modal 0;\n{max_modal_0.shape} are with max and modal 0;\n{min_max_modal_0.shape} are with min, max and modal 0;')
 # OP:whole dataset has (737392, 10) rows;
-# (114, 10) are with 0 min;
-# (611, 10) are with 0 max;
-# (3, 10) are with 0 modal;
+# (65, 10) are with 0 min;
+# (564, 10) are with 0 max;
+# (1, 10) are with 0 modal;
 # (47, 10) are with min and max 0;
 # (2, 10) are with min and modal 0;
 # (0, 10) are with max and modal 0;
+# (0, 10) are with min, max and modal 0;
+
 # problem observed later: when we are counting min_price=0; its also getting counted in min_price=0&modal_price=0 and min_price=0&max_price=0
 # approach steps:
 # 1. if all three 0, delete
@@ -95,9 +97,21 @@ print(f'\nwhole dataset has {ps_df.shape} rows;\n{min_0.shape} are with 0 min;\n
 # 3. if only one is 0, two other strictly !0, handle them
 
 ps_df_cleanup = ps_df
-ps_df_cleanup.loc[ps_df_cleanup['Min_Price']==0.0, 'Min_Price'] = 9.27
-new_min_0 = ps_df_cleanup[ps_df_cleanup['Min_Price']==9.27]
-print(f'rows with min price as 0:\n{new_min_0}')
+
+
+ps_df_cleanup = ps_df_cleanup[
+    ~(
+        (ps_df_cleanup['Min_Price'] == 0.0) &
+        (ps_df_cleanup['Max_Price'] == 0.0) &
+        (ps_df_cleanup['Modal_Price'] == 0.0)
+    )
+]
+print(f'\nnumber of rows with after dropping values where all three prices were 0: {new_ps_df_cleanup.shape}')
+
+
+# ps_df_cleanup.loc[ps_df_cleanup['Min_Price']==0.0, 'Min_Price'] = 9.27
+# new_min_0 = ps_df_cleanup[ps_df_cleanup['Min_Price']==9.27]
+# print(f'rows with min price as 0:\n{new_min_0}')
 
 spark.stop()
 
@@ -114,4 +128,7 @@ what is done until now?
 what answers i am looking for?
 - which mandi is offering the best price for their crop today, and how has that price trended over weeks?
 - which mandis consistently offer the best price for a given commodity, and where do price anomalies (sudden spikes/drops) occur?
+
+VVI TODO: last me 2 alag notes banana - ek approach and kaise decide kiya kya clean karna hai; aur second code ka
+
 '''
